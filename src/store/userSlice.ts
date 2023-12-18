8ab9e8dcb1b9ed4b6ebec6965b906b56121b36a9
Sir1ys <sashakoroviy@gmail.com>
2023-12-18 12:29:26 +0000

    actions for userSlice reducer are updated

--- src/store/userSlice.ts
+++ src/store/userSlice.ts
@@ -1,10 +1,9 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 type User = {
   user_id: number;
   username: string;
   email: string;
-  password: string;
   name: string;
   bio: string;
   avatar_url: string;
@@ -14,7 +13,6 @@ const initialState: User = {
   user_id: 0,
   username: "",
   email: "",
-  password: "",
   name: "",
   bio: "",
   avatar_url: "",
@@ -24,7 +22,18 @@ export const userSlice = createSlice({
   name: "user",
   initialState,
   reducers: {
-    setUser() {},
-    removeUser() {},
+    setUser(state, action: PayloadAction<User>) {
+      state = { ...action.payload };
+    },
+    removeUser(state) {
+      state = {
+        user_id: 0,
+        username: "",
+        email: "",
+        name: "",
+        bio: "",
+        avatar_url: "",
+      };
+    },
   },
 });
