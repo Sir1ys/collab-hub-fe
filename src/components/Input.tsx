3249fe0790c3d405f31695ed6ea8074addfa6877
Sir1ys <sashakoroviy@gmail.com>
2023-12-18 10:32:28 +0000

    useRef setup for input was provided

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,25 +1,28 @@
+import { type ComponentPropsWithoutRef, forwardRef } from "react";
+
 type Props = {
-  type: string;
   label: string;
   id: string;
-};
+} & ComponentPropsWithoutRef<"input">;
 
-export const Input = ({ type, label, id }: Props) => {
-  return (
-    <div className="relative w-72">
-      <label
-        htmlFor={id}
-        className="absolute left-0 px-2.5 text-sky-500 uppercase translate-x-4 translate-y-[-1.375rem] bg-sky-50"
-      >
-        {label}
-      </label>
-      <input
-        type={type}
-        id={id}
-        required
-        placeholder={`Enter ${label}`}
-        className="w-full p-2.5 border-2 border-sky-500 rounded-md outline-none bg-sky-50 text-sky-700 focus:border-sky-700"
-      />
-    </div>
-  );
-};
+export const Input = forwardRef<HTMLInputElement, Props>(
+  ({ label, id, ...props }, ref) => {
+    return (
+      <p className="relative w-72">
+        <label
+          htmlFor={id}
+          className="absolute left-0 px-2.5 text-sky-500 uppercase translate-x-4 translate-y-[-1.375rem] bg-sky-50"
+        >
+          {label}
+        </label>
+        <input
+          id={id}
+          {...props}
+          placeholder={`Enter ${label}`}
+          ref={ref}
+          className="w-full p-2.5 border-2 border-sky-500 rounded-md outline-none bg-sky-50 text-sky-700 focus:border-sky-700"
+        />
+      </p>
+    );
+  }
+);
--- src/components/SignInForm.tsx
+++ src/components/SignInForm.tsx
@@ -1,14 +1,24 @@
+import { useRef } from "react";
 import Button from "./Button";
 import { Input } from "./Input";
 
 type Props = {};
 
 export const SignInForm = (props: Props) => {
+  const emailInput = useRef<HTMLInputElement>(null);
+  const passwordInput = useRef<HTMLInputElement>(null);
+
   return (
     <form className="flex flex-col gap-10">
-      <Input type="email" id="email" label="email" />
-      <Input type="password" id="password" label="password" />
-      <Button text="Sign In"/>
+      <Input type="email" id="email" label="email" required ref={emailInput} />
+      <Input
+        type="password"
+        id="password"
+        label="password"
+        required
+        ref={passwordInput}
+      />
+      <Button text="Sign In" />
     </form>
   );
 };
