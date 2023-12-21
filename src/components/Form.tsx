1f901c0cb8de0c5e7eaef62a8619e07c2140bca0
Sir1ys <sashakoroviy@gmail.com>
2023-12-21 15:24:21 +0000

    responsiveness for the sign-up form is added

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -39,7 +39,7 @@ const Form = forwardRef<FormHandle, Props>(function Form(
 
   return (
     <form
-      className={!signUp ? "flex flex-col gap-10" : "grid grid-cols-2 gap-10"}
+      className={!signUp ? "flex flex-col gap-10 " : "grid grid-cols-1 md:grid-cols-2 gap-10"}
       {...otherProps}
       onSubmit={handleSubmit}
       ref={form}
