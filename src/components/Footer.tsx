b7a5a7a52450f0362ba97ba625ccfa3603cb53e1
Sir1ys <sashakoroviy@gmail.com>
2023-12-21 16:08:31 +0000

    basic footer is created

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,13 @@
-import React from "react";
+import WorkspacesIcon from "@mui/icons-material/Workspaces";
+import Link from "./Link";
 
-type Props = {};
-
-export default function Footer({}: Props) {
-  return <div>Footer</div>;
+export default function Footer() {
+  return (
+    <footer className="p-7 border-t-4 border-sky-700 bg-sky-50 ">
+      <div className=" hidden md:w-max md:flex items-center gap-2 m-auto">
+        <WorkspacesIcon color="primary" />
+        <Link text="Collab Hub" path="/" />
+      </div>
+    </footer>
+  );
 }
