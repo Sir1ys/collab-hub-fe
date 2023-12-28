1ec0c4c812c0e904d4bf29bf2c32a3edc42f35f7
Sir1ys <sashakoroviy@gmail.com>
2023-12-28 11:31:04 +0000

    Link component is refactored, myProjects page for not authorized user is set up

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,15 +1,18 @@
 import { NavLink } from "react-router-dom";
+import { ComponentPropsWithRef } from "react";
 
 type Props = {
   text: string;
   path: string;
-};
+  styles?: string;
+} & ComponentPropsWithRef<"a">;
 
-export default function Link({ text, path }: Props) {
+export default function Link({ text, path, styles, ...props }: Props) {
   return (
     <NavLink
+      {...props}
       to={path}
-      className="text-sky-500 text-2xl font-medium hover:text-sky-700"
+      className={`text-sky-500 text-2xl font-medium hover:text-sky-700 ${styles}`}
     >
       {text}
     </NavLink>
--- src/components/pages/MyProjects.tsx
+++ src/components/pages/MyProjects.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import { useUserSelector } from "../../store/hooks";
+import ArrowRightAltIcon from "@mui/icons-material/ArrowRightAlt";
+import Link from "../Link";
 
 type Props = {};
 
 export default function MyProjects({}: Props) {
-  return <div>MyProjects</div>;
+  const user = useUserSelector((state) => state.user);
+
+  return user.user_id === 0 ? (
+    <h2 className="text-sky-700 text-xl font-semibold md:text-2xl">
+      Log In to see your projects
+      <ArrowRightAltIcon className="m-2" />
+      <Link text="Log In" path="/login" styles="text-xl md:text-2xl" />
+    </h2>
+  ) : (
+    <></>
+  );
 }
