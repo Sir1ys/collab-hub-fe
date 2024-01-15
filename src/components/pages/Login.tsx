dca90d76fe78c58d7e99e84f39417dbeb8ad61c1
Sir1ys <sashakoroviy@gmail.com>
2024-01-15 11:32:51 +0000

    a separate component was created for SignUp form

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,66 +1,19 @@
-import { useRef, useState, FormEvent } from "react";
+import { useRef, useState } from "react";
 import { useUserDispatch } from "../../store/hooks";
 import { setUser } from "../../store/userSlice";
 import Form, { type FormHandle } from "../Form";
 import Button from "../Button";
 import { Input } from "../Input";
 import { type User } from "../../types/types";
-import { createUser, getUserByEmail } from "../../utils/users_api";
+import { getUserByEmail } from "../../utils/users_api";
 import { useNavigate } from "react-router-dom";
-import useMultiStepForm from "../../hooks/useMultiStepForm";
-import UserDetailsForm from "../UserDetailsForm";
-import OtherDetailsForm from "../OtherDetailsForm";
-
-type UserData = {
-  email: string;
-  bio: string;
-  name: string;
-  username: string;
-  avatar_url: string;
-  github_url: string;
-  password: string;
-};
-
-const initial_data: UserData = {
-  email: "",
-  bio: "",
-  name: "",
-  username: "",
-  avatar_url: "",
-  github_url: "",
-  password: "",
-};
+import SignUpForm from "../SignUpForm";
 
 export default function Login() {
-  const [data, setData] = useState(initial_data);
   const [signIn, setSignIn] = useState(true);
   const navigate = useNavigate();
   const dispatch = useUserDispatch();
   const signInForm = useRef<FormHandle>(null);
-  const signUpForm = useRef<FormHandle>(null);
-
-  const { steps, step, currentStepIndex, isFirstStep, isLastStep, back, next } =
-    useMultiStepForm([
-      <UserDetailsForm
-        name={data.name}
-        username={data.username}
-        email={data.email}
-        password={data.password}
-        updateFields={updateFields}
-      />,
-      <OtherDetailsForm
-        avatar_url={data.avatar_url}
-        bio={data.bio}
-        github_url={data.github_url}
-        updateFields={updateFields}
-      />,
-    ]);
-
-  function updateFields(fields: Partial<User>) {
-    setData((prev) => {
-      return { ...prev, ...fields };
-    });
-  }
 
   function handleSignIn(data: unknown) {
     const extractedData = data as { email: string; password: string };
@@ -84,21 +37,6 @@ export default function Login() {
     signInForm.current?.clear();
   }
 
-  function handleSingUp(event: FormEvent) {
-    event.preventDefault();
-
-    if (!isLastStep) return next();
-
-    createUser({
-      user: data,
-    }).then((user: User) => {
-      dispatch(setUser(user));
-      navigate("/");
-    });
-
-    signUpForm.current?.clear();
-  }
-
   return (
     <div className="relative p-10">
       {signIn ? (
@@ -113,33 +51,7 @@ export default function Login() {
         </Form>
       ) : (
         <>
-          <div className="absolute top-0 right-10 text-sky-800 font-medium">
-            {currentStepIndex + 1} / {steps.length}
-          </div>
-          <form onSubmit={handleSingUp} className="flex flex-col gap-6">
-            {step}
-            <div className="flex gap-2 justify-end">
-              {isFirstStep && (
-                <Button
-                  text="Next"
-                  styles={"w-24"}
-                  onSubmit={handleSingUp}
-                  type="submit"
-                />
-              )}
-              {isLastStep && (
-                <>
-                  <Button
-                    text="Back"
-                    styles={"w-24"}
-                    onClick={back}
-                    type="button"
-                  />
-                  <Button text="Sign Up" styles={"w-24"} type="submit" />
-                </>
-              )}
-            </div>
-          </form>
+          <SignUpForm />
         </>
       )}
       <p
